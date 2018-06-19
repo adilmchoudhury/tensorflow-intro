e031c3114e3ea0d2e3406886cb35a0a8e8e51c7a
Print <adil@Dcts-MacBook-Pro.local>
2018-06-20 03:21:38 +0600

    exploring tensorflow basic

--- intro.py
+++ intro.py
@@ -1,1 +1,30 @@
-print ("hello world");
+# Import `tensorflow`
+import tensorflow as tf
+
+# Initialize two constants
+x1 = tf.constant([1,2,3,4])
+x2 = tf.constant([5,6,7,8])
+
+# Multiply
+result = tf.multiply(x1, x2)
+
+
+
+### One way of initializing the Session
+
+# Intialize the Session
+sess = tf.Session()
+
+# Print the result
+print(sess.run(result))
+
+# Close the session
+sess.close()
+
+
+### another way of using the default Session
+
+# Initialize Session and run 'result'
+with tf.Session() as sess:
+  output = sess.run(result)
+  print(output)
